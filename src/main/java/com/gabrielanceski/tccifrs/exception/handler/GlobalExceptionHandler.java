3497f734e07d35f9f0a0a62eb2f49fcd02d2dbfe
Gabriel Anceski <gabrielanceski@gmail.com>
2024-09-22 13:44:11 -0300

    feat: data integrity violation handlers

--- src/main/java/com/gabrielanceski/tccifrs/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/gabrielanceski/tccifrs/exception/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.gabrielanceski.tccifrs.exception.InvalidDataException;
 import com.gabrielanceski.tccifrs.presentation.domain.response.FaultResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @RestControllerAdvice
 @Slf4j
@@ -35,6 +37,17 @@ public class GlobalExceptionHandler {
                 new FaultResponse(
                         "Internal server error",
                         HttpStatus.INTERNAL_SERVER_ERROR.value()
+                )
+        );
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class, SQLIntegrityConstraintViolationException.class})
+    public final ResponseEntity<Object> handleDataIntegrityViolation(Exception exception, WebRequest request) {
+        log.error("handleDataIntegrityViolation() - message <{}> - error: ", exception.getMessage(), exception);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                new FaultResponse(
+                        "Duplicate entry",
+                        HttpStatus.CONFLICT.value()
                 )
         );
     }
