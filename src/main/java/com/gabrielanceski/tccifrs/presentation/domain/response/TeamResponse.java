c071357000568c240b645d3f6e1afa277dc8ff02
Gabriel Anceski <gabrielanceski@gmail.com>
2024-09-23 23:27:54 -0300

    fix: include not null fields

--- src/main/java/com/gabrielanceski/tccifrs/presentation/domain/response/TeamResponse.java
+++ src/main/java/com/gabrielanceski/tccifrs/presentation/domain/response/TeamResponse.java
@@ -1,5 +1,6 @@
 package com.gabrielanceski.tccifrs.presentation.domain.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gabrielanceski.tccifrs.domain.entity.Project;
 import com.gabrielanceski.tccifrs.domain.entity.Team;
@@ -8,6 +9,7 @@ import com.gabrielanceski.tccifrs.domain.entity.User;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record TeamResponse(
     String id,
     String name,
