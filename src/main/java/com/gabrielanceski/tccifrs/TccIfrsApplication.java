aa8db494ca54d1daf29ddf93f2a194f3d3dac4fc
Gabriel Anceski <gabrielanceski@gmail.com>
2024-09-23 22:15:20 -0300

    feat: jpa auditing for entity timestamps

--- src/main/java/com/gabrielanceski/tccifrs/TccIfrsApplication.java
+++ src/main/java/com/gabrielanceski/tccifrs/TccIfrsApplication.java
@@ -2,8 +2,10 @@ package com.gabrielanceski.tccifrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TccIfrsApplication {
 
     public static void main(String[] args) {
